457a6b7a546dc3870012501f15c4015bf02b6a2a
h1nttt61 <Lyl1.32@yandex.ru>
2026-03-23 23:17:33 +0300

    feat(domain): add user additional data

--- domain/src/main/java/com/financial/loan/domain/entity/UserAdditionalData.java
+++ domain/src/main/java/com/financial/loan/domain/entity/UserAdditionalData.java
@@ -1,4 +1,44 @@
 package com.financial.loan.domain.entity;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserAdditionalData {
+    private final UUID id;
+    private final UUID userId;
+    private final Timestamp birthday;
+    private final String password;
+    private final Double monthlyIncome;
+    private final Timestamp createdAt;
+    private final Timestamp updatedAt;
+
+    public static UserAdditionalData create(UUID userId,
+                                            Timestamp birthday,
+                                            String password,
+                                            Double monthlyIncome){
+        if (userId == null){
+            throw new IllegalArgumentException("userId cannot be null");
+        }
+        if (password == null || password.isBlank()){
+            throw new IllegalArgumentException("password cannot be blank");
+        }
+        if (monthlyIncome != null && monthlyIncome < 0){
+            throw new IllegalArgumentException("monthlyIncome cannot be negative");
+        }
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+
+        return new UserAdditionalData(UUID.randomUUID(),
+                userId,
+                birthday,
+                password,
+                monthlyIncome,
+                now,
+                now);
+    }
 }
