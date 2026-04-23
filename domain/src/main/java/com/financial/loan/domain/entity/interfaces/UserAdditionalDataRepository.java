ccc182b5d409ecc1221c1fea1cdec7d3413b2f05
RePti-LoiD <delhikovsr@gmail.com>
2026-04-23 11:57:58 +0300

    refactor(domain): fix repository signatures

--- domain/src/main/java/com/financial/loan/domain/entity/interfaces/UserAdditionalDataRepository.java
+++ domain/src/main/java/com/financial/loan/domain/entity/interfaces/UserAdditionalDataRepository.java
@@ -1,19 +1,22 @@
 package com.financial.loan.domain.entity.interfaces;
 
+import com.financial.loan.domain.entity.UserAdditionalData;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface UserAdditionalDataRepository {
-    UserAdditionalDataRepository getById(UUID userAdditionalDataId);
+    UserAdditionalData getById(UUID userId);
 
-    UUID createUserAdditionalData(UserAdditionalDataRepository entity);
-
-    UUID updateUserAdditionalData(
-            UUID userId,
+    UUID createUserAdditionalData(
             LocalDateTime birthday,
-            String password,
+            String passwordHash,
             BigDecimal monthlyIncome
+    );
+
+    UserAdditionalData updateUserAdditionalData(
+        UserAdditionalData entity
     );
 
     UUID deleteUserAdditionalData(UUID userAdditionalDataId);
