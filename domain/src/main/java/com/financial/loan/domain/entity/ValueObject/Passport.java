3297a63fdc45db1e427f537ab37b1e4b18b1b7e0
Arseny116 <Olifirov24@gmail.com>
2026-04-25 18:16:22 +0300

    Update Passport.java

--- domain/src/main/java/com/financial/loan/domain/entity/ValueObject/Passport.java
+++ domain/src/main/java/com/financial/loan/domain/entity/ValueObject/Passport.java
@@ -1,12 +1,3 @@
 package com.financial.loan.domain.entity.ValueObject;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class Passport {
-    String series ;
-    String number ;
-}
+public record Passport(String series, String number) {}
